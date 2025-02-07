acc9a172ac4d23de99636930b534ebb079f669de
Emmilija <karatashevskaemilija@gmail.com>
2025-02-07 12:05:00 +0100

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -6,12 +6,19 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
+  
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: __dirname,
     },
     plugins: {
       'react-hooks': reactHooks,
