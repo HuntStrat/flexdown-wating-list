aae4769decff1cd18e109c6e94bd50db114da090
Kwasi <stawuah18@gmail.com>
2025-02-11 10:26:59 +0000

    fix build on waiting list

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from "path"
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+  
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
-// https://vitejs.dev/config/
+import { fileURLToPath } from 'url'
+
+// Create the __dirname equivalent for ES modules
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  
 })
