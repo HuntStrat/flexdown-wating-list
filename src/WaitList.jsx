992f26e57b44c74eefa1b65a44b952aeb1498adc
Sandy Abasman <abasifrekesandy@gmail.com>
2025-03-08 21:49:27 +0200

    waitlist

---
+++ src/WaitList.jsx
@@ -0,0 +1,5 @@
+const WaitList = () => {
+  return <div>WaitList</div>;
+};
+
+export default WaitList;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,59 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-    colors: {
-      'dark-black': '#161617',
-      'light-black': '#242629',
-      'black-gray': '#3A3C3F',
-      'dark-gray': '#4D5258',
-      'light-gray' : '#595C61',
-      'border-gray' : '#AAACAE',
-      'gray': '#86888B',
-      'white' : '#F5F5F8',
-      'blur-white': '#F9F7FF',
-      'gradient-purple': '#9976DC63',
-      'gradient-white': '#FFFFFF',
-   
-      
-            },
-            borderRadius: {
-              '2px': '2px', 
-            },
-            blur: {
-              'custom-blur': '10px',
-            },
-            boxShadow: {
-              'list-shadow': '0px 4px 10px rgba(0, 0, 0, 0.1)',
-            },
-            backgroundColor: {
-              'border-gradient':
-                'linear-gradient(90deg, #9976DC63 0%, #FFFFFF 100%)',
-            },
- 
-      lineHeight: {
-        '10': '60px',
-        '20': '90px',
-        '36': '36px',
-      },
-      backgroundImage: {
-        'custom-bg': "url('/src/assets/images/BG IMAGE.png')",
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-    },
-  },
-
-  screens: {
-    sm: '480px',
-    md: '768px',
-    lg: '976px',
-    xl: '1440px',
-    desktop1024: { max: '1024px' },
+    extend: {},
   },
   plugins: [],
-}
-
-
+};
